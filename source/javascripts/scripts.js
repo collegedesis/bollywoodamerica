b400ca60eae37a40b622a7df1dbb68553213c47c
Mehul Kar <mehul.kar@gmail.com>
2013-09-02 21:00:05 -0700

    add bids

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -46,10 +46,8 @@ jQuery(document).ready(function($){
 
       $('.image-wrapper').hover(function(){
         $(this).find('.mouse-effect').stop().animate({'opacity':'0.6'});
-        $(this).find('.extra-links').stop().animate({'top':'50%'});
          },function(){
          $(this).find('.mouse-effect').stop().animate({'opacity':'0'});
-        $(this).find('.extra-links').stop().animate({'top':'-50%'});
       });
     });
   });
